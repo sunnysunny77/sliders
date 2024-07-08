bad292e1441c625f67f3ffbca62bc73d4d5c1ccc
daniel <contractor@lateralaspect.com.au>
2024-07-09 02:43:09 +0800

    up

--- mkcert.sh
+++ mkcert.sh
@@ -2,10 +2,10 @@
 if [ -e "server.crt" ] && [ -e "server.key" ] ; then
     echo "sll exist's"
 else 
-openssl genrsa -out server.key &&
-openssl req -new -key server.key -out server.csr -config csr.conf &&
-openssl x509 -req -days 365 -in server.csr -signkey server.key -out server.crt -extensions req_ext -extfile csr.conf &&
-openssl req -in self-ssl.csr -text -noout &&
-openssl x509 -in self-ssl.crt -text -noout;
+
+openssl genrsa -out ca.key 2048 &&
+openssl req -x509 -new -nodes -key ca.key -sha256 -days 365 -out ca.crt -subj '/CN=dev.localhost Root CA/C=AU/ST=Western Australia/L=Perth/O=Web Development' &&
+openssl req -new -nodes -out server.csr -newkey rsa:4096 -keyout server.key -subj '/CN=dev.localhost/C=AU/ST=Western Australia/L=Perth/O=Web Development' &&
+openssl x509 -req -in server.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out server.crt -days 730 -sha256 -extfile ssl.conf;
 npm run install-cert
 fi 
