714c332a75d828ff98692214335a8d3efcc05db3
daniel <contractor@lateralaspect.com.au>
2024-07-09 00:41:22 +0800

    up

--- install.sh
+++ install.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-go install github.com/caddyserver/xcaddy/cmd/xcaddy@latest && xcaddy build && sudo chmod 777 caddy && npm run mkcert;
+go install github.com/caddyserver/xcaddy/cmd/xcaddy@latest && xcaddy build && npm run mkcert;
