930a914696c89e2457cbb95f991fc49069e5cda0
daniel <contractor@lateralaspect.com.au>
2024-07-09 11:15:16 +0800

    up

--- mkcert.sh
+++ mkcert.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 if [ -e "server.crt" ] && [ -e "server.key" ] ; then
+
     echo "sll exist's"
 else 
 
-openssl req -nodes -newkey rsa:2048 -keyout private.key -out ca.csr -subj '/CN=dev.localhost Root CA/C=AU/ST=Western Australia/L=Perth/O=Web Development' &&
-openssl x509 -in ca.csr -out server.crt -req -signkey private.key -days 365 -subj '/CN=dev.localhost/C=AU/ST=Western Australia/L=Perth/O=Web Development' -extfile ssl.conf;
+openssl genrsa -out ca.key 2048 &&
+openssl req -x509 -new -nodes -key ca.key -days 1825 -out ca.crt -subj '/CN=dev.localhost Root CA/C=AU/ST=Western Australia/L=Perth/O=Web Development' &&
+
+openssl genrsa -out server.key 2048 &&
+openssl req -new -key server.key -out server.csr -subj '/CN=dev.localhost/C=AU/ST=Western Australia/L=Perth/O=Web Development' &&
+openssl x509 -req -in server.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out server.crt -days 825 -extfile ssl.conf;
+
 npm run install-cert
 fi 
--- rollup.config-watch.mjs
+++ rollup.config-watch.mjs
@@ -20,7 +20,7 @@ export default {
       watch: "./site",
       port: 3000,
       https: {
-        key: fs.readFileSync("./private.key"),
+        key: fs.readFileSync("./server.key"),
         cert: fs.readFileSync("./server.crt")
       }
     })
