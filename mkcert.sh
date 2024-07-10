17eef488124c37d6f6ae6e0c40bc754d72a14fd1
daniel <contractor@lateralaspect.com.au>
2024-07-10 13:34:32 +0800

    up

--- mkcert.sh
+++ mkcert.sh
@@ -3,7 +3,7 @@ if [ -e "server.crt" ] && [ -e "server.key" ] ; then
 
     echo "sll exist's"
 else 
-openssl req -x509 -sha256 -days 356 -nodes -newkey rsa:2048 -subj "/CN=dev.localhost/C=AU/L=Perth" -keyout ca.key -out ca.crt
+openssl req -x509 -sha256 -days 356 -nodes -newkey rsa:2048  -subj '/CN=dev.localhost Root CA/C=AU/ST=Western Australia/L=Perth/O=Web Developers/OU=Dev' -keyout ca.key -out ca.crt
 openssl genrsa -out server.key 2048
 openssl req -new -key server.key -out server.csr -config csr.conf
 openssl x509 -req -in server.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out server.crt -days 365 -sha256 -extfile cert.conf
--- open.js
+++ open.js
@@ -1,3 +1,3 @@
 import open from "open";
-await open("https://dev.localhost:3000");
-console.log("running on https://dev.localhost:3000");
+await open("http://localhost:3003");
+console.log("running on http://localhost:3003");
