563753192bcfbdb2dfd6aff086f2bcae2700750a
daniel <contractor@lateralaspect.com.au>
2024-12-05 14:35:55 +0800

    Update slider_9.js

--- js/slider_9.js
+++ js/slider_9.js
@@ -3,13 +3,11 @@ import { events } from "./utillites.js";
 export const slider_9 = () => {
 
   const counters = document.querySelectorAll(".counters");
-  const inner = document.querySelector(".inner-swap");
   const prev = document.querySelector(".button-prev");
   const next = document.querySelector(".button-next");
 
   if (
     counters.length === 0 ||
-    !inner ||
     !prev ||
     !next
   ) {
@@ -19,63 +17,57 @@ export const slider_9 = () => {
   let index = 0;
   const length = counters.length;
 
-  events(prev, "click", () => {
+  const first = (index) => {
 
-    const current = index;
-
-    counters[current].classList.add("has-fade");
+    counters[index].classList.add("has-fade");
 
     setTimeout(()=>{
 
-      counters[current].classList.remove("has-current");
-      counters[current].classList.remove("has-fade");
+      counters[index].classList.remove("has-current");
+      counters[index].classList.remove("has-fade");
     },100);
+  };
 
-    index--;
+  const last = (index) => {
+
+    setTimeout(()=>{
+
+      counters[index].classList.add("has-current");
+      counters[index].classList.add("has-fade");
+      setTimeout(()=>{
+
+        counters[index].classList.remove("has-fade");
+      },100);
+    },100);
+  };
+
+  events(prev, "click", () => {
+
+    first(index);
 
     if (index === - 1) {
 
       index = length - 1;
+    } else {
+
+      index--;
     }
 
-    setTimeout(()=>{
-
-      counters[index].classList.add("has-current");
-      counters[index].classList.add("has-fade");
-      setTimeout(()=>{
-
-        counters[index].classList.remove("has-fade");
-      },100);
-    },100);
+    last(index);
   });
 
   events(next, "click", () => {
 
-    const current = index;
-
-    counters[current].classList.add("has-fade");
-
-    setTimeout(()=>{
-
-      counters[current].classList.remove("has-current");
-      counters[current].classList.remove("has-fade");
-    },100);
-
-    index++;
+    first(index);
 
     if (index === length) {
 
       index = 0;
+    } else {
+
+      index++;
     }
 
-    setTimeout(()=>{
-
-      counters[index].classList.add("has-current");
-      counters[index].classList.add("has-fade");
-      setTimeout(()=>{
-
-        counters[index].classList.remove("has-fade");
-      },100);
-    },100);
+    last(index);
   });
 };
